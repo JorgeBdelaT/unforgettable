f684ad9bc1d9ac3c2a6d20a8a3e384bd41597ba0
jorge <jorgebdelat@uber.com>
2022-10-21 13:07:10 -0300

    chore: add task and reminder models

---
+++ prisma/migrations/20221021154020_add_task_and_reminder_models/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "deadline" TIMESTAMP(3),
+    "priority" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reminder" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "priority" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Reminder_pkey" PRIMARY KEY ("id")
+);
