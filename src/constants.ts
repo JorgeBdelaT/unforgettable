8778a40a2ec01b9008e7c0be8497f95b51700d74
jorge <jorgebdelat@uber.com>
2022-11-10 13:55:04 -0300

    feat(task-list-item): display completedAt

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export const DATE_TIME_FORMAT = "HH:mm DD.MM";
 export const GET_ALL_TASKS_QUERY_KEY = [["tasks", "getAll"]];
 export const HEADER_HEIGHT = "3rem";
 export const TASKS_LIST_ID = "tasks-list";
+export const TIME_FORMAT = "HH:mm";
 export const UNDO_LAST_TASK_REMOVAL_MUTATION_KEY = ["undoLastTaskRemoval"];
