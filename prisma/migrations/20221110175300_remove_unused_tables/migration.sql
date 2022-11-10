aa7c26533edcb0bd338eb1b5b2e0c41666c391e9
jorge <jorgebdelat@uber.com>
2022-11-10 15:04:09 -0300

    chore: remove unused tables

---
+++ prisma/migrations/20221110175300_remove_unused_tables/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Record` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Reminder` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Record";
+
+-- DropTable
+DROP TABLE "Reminder";
