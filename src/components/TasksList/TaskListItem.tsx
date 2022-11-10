cca16b67bc972d348b02a071bc315ed73a408fb1
jorge <jorgebdelat@uber.com>
2022-11-09 23:20:22 -0300

    feat: add toggle logic

--- src/components/TasksList/TaskListItem.tsx
+++ src/components/TasksList/TaskListItem.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
-import { Task } from "@prisma/client";
 import { XMarkIcon } from "@heroicons/react/24/solid";
+
+import { Task } from "@prisma/client";
 import { trpc } from "../../utils/trpc";
 import { useQueryClient } from "@tanstack/react-query";
-import { GET_ALL_TASKS_QUERY_KEY } from "../../constants";
+
+import { DATE_TIME_FORMAT, GET_ALL_TASKS_QUERY_KEY } from "../../constants";
+import moment from "moment";
 
 interface TaskListItemProps {
   task: Task;
@@ -11,8 +14,12 @@ interface TaskListItemProps {
 
 const TaskListItem: FC<TaskListItemProps> = ({ task }) => {
   const queryClient = useQueryClient();
-  const { mutate, isLoading } = trpc.tasks.delete.useMutation({
-    onMutate: async ({ taskId: deletedTaskId }) => {
+
+  const {
+    mutate: toggleTaskStatusMutation,
+    isLoading: toggleTaskStatusLoading,
+  } = trpc.tasks.toggleCompletedState.useMutation({
+    onMutate: async ({ taskId: toggledTaskId }) => {
       const previousTasks = queryClient.getQueryData<Task[]>(
         GET_ALL_TASKS_QUERY_KEY
       );
@@ -21,7 +28,11 @@ const TaskListItem: FC<TaskListItemProps> = ({ task }) => {
         GET_ALL_TASKS_QUERY_KEY,
         (old: Task[] | undefined) => {
           if (!old) return [];
-          return old.filter(({ id }) => id !== deletedTaskId);
+          return old.map((task) =>
+            task.id === toggledTaskId
+              ? { ...task, completed: !task.completed }
+              : task
+          );
         }
       );
 
@@ -29,19 +40,79 @@ const TaskListItem: FC<TaskListItemProps> = ({ task }) => {
     },
     onError: (err, _, context) => {
       // TODO: do something better with the error
-      window.alert(`No se pudo eliminar la tarea :( ${err.message}`);
+      window.alert(
+        `No se pudo cambiar el estado de la tarea :( ${err.message}`
+      );
 
       queryClient.setQueryData(GET_ALL_TASKS_QUERY_KEY, context?.previousTasks);
     },
   });
 
+  const { mutate: deleteTaskMutation, isLoading: deleteTaskMutationLoading } =
+    trpc.tasks.delete.useMutation({
+      onMutate: async ({ taskId: deletedTaskId }) => {
+        const previousTasks = queryClient.getQueryData<Task[]>(
+          GET_ALL_TASKS_QUERY_KEY
+        );
+
+        queryClient.setQueryData(
+          GET_ALL_TASKS_QUERY_KEY,
+          (old: Task[] | undefined) => {
+            if (!old) return [];
+            return old.filter(({ id }) => id !== deletedTaskId);
+          }
+        );
+
+        return { previousTasks };
+      },
+      onError: (err, _, context) => {
+        // TODO: do something better with the error
+        window.alert(`No se pudo eliminar la tarea :( ${err.message}`);
+
+        queryClient.setQueryData(
+          GET_ALL_TASKS_QUERY_KEY,
+          context?.previousTasks
+        );
+      },
+    });
+
   const handleDelete = () => {
-    if (!isLoading) mutate({ taskId: task.id });
+    if (!deleteTaskMutationLoading) deleteTaskMutation({ taskId: task.id });
   };
 
+  const handleToggleTaskStatus = () => {
+    if (!toggleTaskStatusLoading) toggleTaskStatusMutation({ taskId: task.id });
+  };
+
+  const dateTimeToDisplay = moment(task.createdAt).format(DATE_TIME_FORMAT);
+
+  const { completed } = task;
+
   return (
-    <li className="relative mb-4 rounded-lg bg-slate-800 py-2 px-4 shadow-md transition-colors last:mb-0 hover:bg-opacity-90">
-      {task.text}
+    <li
+      className={`relative mb-4 flex flex-col rounded-lg bg-slate-800 py-2 px-4 pr-6 shadow-md transition-colors last:mb-0 ${
+        completed ? "bg-opacity-70" : "hover:bg-opacity-90"
+      }`}
+    >
+      <div className="container mt-1 flex items-start gap-4">
+        <input
+          onChange={handleToggleTaskStatus}
+          value={`${completed}`}
+          checked={completed}
+          type="checkbox"
+          className="relative flex h-6 w-6 flex-shrink-0 cursor-pointer appearance-none items-center justify-center rounded-xl bg-indigo-200  checked:before:block checked:before:h-4 checked:before:w-4 checked:before:rounded-full checked:before:bg-indigo-400"
+        />
+        <p
+          className={`${
+            completed ? "text-gray-400 line-through" : ""
+          } decoration-indigo-400 decoration-2`}
+        >
+          {task.text}
+        </p>
+      </div>
+      <span className="self-end text-xs font-light text-gray-300">
+        {dateTimeToDisplay}
+      </span>
       <button
         onClick={handleDelete}
         className="absolute top-0 right-0 flex h-8 w-8 justify-end"
