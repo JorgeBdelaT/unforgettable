f33a98ec78170e3bf3c84422c5336b49819ed9f0
jorge <jorgebdelat@uber.com>
2022-11-10 16:04:21 -0300

    feat: add sign out button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import CreateTask from "./CreateTask";
 import Header from "./Header";
 import Layout from "./Layout";
+import SignOutBtn from "./SignOutBtn";
 import { TasksList, TasksListSkeleton } from "./TasksList";
 import ToggleCompletedTasksVisibilityBtn from "./ToggleCompletedTasksVisibilityBtn";
 import UndoLastTaskRemovalBtn from "./UndoLastTaskRemovalBtn";
@@ -9,6 +10,7 @@ export {
   CreateTask,
   Header,
   Layout,
+  SignOutBtn,
   TasksList,
   TasksListSkeleton,
   ToggleCompletedTasksVisibilityBtn,
