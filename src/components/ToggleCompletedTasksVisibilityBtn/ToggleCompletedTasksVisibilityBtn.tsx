56f7cccc2e71be503fbc5b2fc7ead5f8d81657d1
jorge <jorgebdelat@uber.com>
2022-11-10 13:10:34 -0300

    fix: persist user settings with localStorage

--- src/components/ToggleCompletedTasksVisibilityBtn/ToggleCompletedTasksVisibilityBtn.tsx
+++ src/components/ToggleCompletedTasksVisibilityBtn/ToggleCompletedTasksVisibilityBtn.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 
 import useSettingsStore from "../../stores/SettingsStore";
@@ -7,16 +7,23 @@ const ToggleCompletedTasksVisibilityBtn = () => {
   const { displayCompletedTasks, toggleDisplayCompletedTasks } =
     useSettingsStore();
 
+  const [iconToDisplay, setIconToDisplay] = useState<JSX.Element>();
+
+  useEffect(() => {
+    setIconToDisplay(
+      displayCompletedTasks ? (
+        <EyeIcon className="h-6 w-6" />
+      ) : (
+        <EyeSlashIcon className="h-6 w-6" />
+      )
+    );
+  }, [displayCompletedTasks]);
   return (
     <button
       onClick={toggleDisplayCompletedTasks}
       className="rounded-md bg-indigo-200 p-2 text-slate-800 opacity-90 hover:bg-indigo-300 disabled:pointer-events-none disabled:text-slate-500"
     >
-      {displayCompletedTasks ? (
-        <EyeIcon className="h-6 w-6" />
-      ) : (
-        <EyeSlashIcon className="h-6 w-6" />
-      )}
+      {iconToDisplay}
     </button>
   );
 };
