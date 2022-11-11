907b7abf3d5d950b48d1fc76cc6d05d2e7d4c32d
jorge <jorgebdelat@uber.com>
2022-11-11 18:00:17 -0300

    fix(lists-page): reset selected list on mount

--- src/pages/lists/index.tsx
+++ src/pages/lists/index.tsx
@@ -1,11 +1,10 @@
+import { useEffect } from "react";
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-
 import { unstable_getServerSession as getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]";
-
-import { Header, SignOutBtn, UserLists } from "../../components";
-
+import { Header, SignOutBtn, ListsGrid } from "../../components";
+import useSelectedListStore from "../../stores/SelectedListStore";
 import { ROUTES } from "../../constants";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
@@ -24,6 +23,14 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 };
 
 const Lists: NextPage = () => {
+  const setSelectedList = useSelectedListStore(
+    (state) => state.setSelectedList
+  );
+
+  useEffect(() => {
+    setSelectedList(undefined);
+  }, [setSelectedList]);
+
   return (
     <>
       <Head>
@@ -34,7 +41,7 @@ const Lists: NextPage = () => {
 
       <section className="flex h-full flex-col">
         <Header title="Listas" actions={<SignOutBtn />} />
-        <UserLists />
+        <ListsGrid />
         {/* TODO: <CreateList /> */}
       </section>
     </>
