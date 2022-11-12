9a46959baee8e365f89a01170f65fc95427c47af
jorge <jorgebdelat@uber.com>
2022-11-11 21:27:51 -0300

    feat(task-router): add list id

--- src/server/trpc/router/task.ts
+++ src/server/trpc/router/task.ts
@@ -3,25 +3,19 @@ import { z } from "zod";
 import { Task } from "@prisma/client";
 import moment from "moment";
 
-const TEXT_MIN_LENGTH = 5;
-
 export const taskRouter = router({
   create: publicProcedure
     .input(
       z.object({
-        text: z
-          .string()
-          .trim()
-          .min(TEXT_MIN_LENGTH, {
-            message: `Record must be at least ${TEXT_MIN_LENGTH} characters long.`,
-          }),
+        text: z.string().trim(),
         deadline: z
           .date()
           .min(new Date(), { message: "A deadline must be greater than today" })
           .nullish(),
+        listId: z.string().cuid(),
       })
     )
-    .mutation(async ({ input: { text, deadline }, ctx }) => {
+    .mutation(async ({ input: { text, deadline, listId }, ctx }) => {
       const priority =
         (await ctx.prisma.task.findMany()).reduce(
           (maxPriority, { priority }) => {
@@ -38,43 +32,37 @@ export const taskRouter = router({
           text,
           deadline,
           priority,
+          listId,
         },
       });
     }),
 
   delete: publicProcedure
     .input(z.object({ taskId: z.string().cuid() }))
-    .mutation(({ ctx, input: { taskId } }) => {
-      return ctx.prisma.task.update({
+    .mutation(async ({ ctx, input: { taskId } }) => {
+      await ctx.prisma.task.update({
         where: { id: taskId },
         data: { deletedAt: new Date() },
       });
     }),
 
-  undoLastRemoval: publicProcedure.mutation(async ({ ctx }) => {
-    const latestRemoval = (await ctx.prisma.task.findMany()).reduce(
-      (acc, curr) => {
-        // TODO: redactor
-        if (
-          (acc?.deletedAt &&
-            curr?.deletedAt &&
-            curr.deletedAt > acc.deletedAt) ||
-          !acc
-        ) {
-          acc = curr;
-        }
-        return acc;
-      },
-      null as Task | null
-    );
+  undoLastRemoval: publicProcedure
+    .input(z.object({ listId: z.string().cuid() }))
+    .mutation(async ({ ctx, input: { listId } }) => {
+      const latestRemoval = (
+        await ctx.prisma.task.findMany({
+          where: { listId, deletedAt: { not: null } },
+          orderBy: { deletedAt: "desc" },
+        })
+      )[0];
 
-    if (latestRemoval) {
-      return ctx.prisma.task.update({
-        where: { id: latestRemoval.id },
-        data: { deletedAt: null },
-      });
-    }
-  }),
+      if (latestRemoval) {
+        await ctx.prisma.task.update({
+          where: { id: latestRemoval.id },
+          data: { deletedAt: null },
+        });
+      }
+    }),
 
   toggleCompletedState: publicProcedure
     .input(z.object({ taskId: z.string().cuid() }))
@@ -101,16 +89,20 @@ export const taskRouter = router({
       });
     }),
 
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const maxCompletedDate = moment(new Date()).subtract(24, "h").toDate();
-    const allTasks = await ctx.prisma.task.findMany({
-      where: { deletedAt: null },
-      orderBy: { priority: "desc" },
-    });
-    return allTasks.filter(
-      ({ completed, completedAt }) =>
-        !completed ||
-        (completed && completedAt && completedAt >= maxCompletedDate)
-    );
-  }),
+  getAll: publicProcedure
+    .input(z.object({ listId: z.string().cuid().nullish() }))
+    .query(async ({ ctx, input: { listId } }) => {
+      if (listId) {
+        const maxCompletedDate = moment(new Date()).subtract(24, "h").toDate();
+        const allTasks = await ctx.prisma.task.findMany({
+          where: { deletedAt: null, listId },
+          orderBy: { priority: "desc" },
+        });
+        return allTasks.filter(
+          ({ completed, completedAt }) =>
+            !completed ||
+            (completed && completedAt && completedAt >= maxCompletedDate)
+        );
+      }
+    }),
 });
